ca3c025187ab88c78e2ca8b4844541f631e1bdba
vyylencee <gultomevi22@gmail.com>
2026-09-17 09:57:39 +0700

    recreate button

--- pages/6_Upload Data.py
+++ pages/6_Upload Data.py
@@ -18,6 +18,7 @@ st.title("⬆️ Upload Data Production")
 st.divider()
 
 file = st.file_uploader("Upload Data dengan format .xlsx, .xls", type=["xlsx", "xls"])
+button_kirim = st.button("Upload Data")
 try :
     OilGasData = pd.read_excel(file, sheet_name="PROD")
     wipData = pd.read_excel(file, sheet_name="SUMUR INJEKSI")
@@ -27,21 +28,8 @@ try :
     wipData = save_data(wipData, "DATE")
     wellData = save_data(wellData, "Date")
 
-    st.button("Upload Data", on_click=save_data_to_google_sheets, args=(OilGasData, wipData, wellData, "tabelOilGas", "tabelWip", "tabelWell"))
-    st.success("✅ Data berhasil diupload ke Google Sheets")
-
-    dataLog = pd.DataFrame([{
-                "Waktu Upload" : waktuUpload,
-                "Production Oil & Gas" : "Sheet Oil Gas",
-                "WIP" : "Sheet WIP",
-                "Well" : "Sheet Well",
-                "Status" : "✅ Berhasil"
-            }])
-    
-    if os.path.exists(PATH_LOG) :
-        dataLog.to_csv("assets/log upload.csv", index=False, mode="a", header=False)
-    else :
-        dataLog.to_csv("assets/log upload.csv", index=False, mode="w")
+    if button_kirim :
+        save_data_to_google_sheets(OilGasData, wipData, wellData, "tabelOilGas", "tabelWip", "tabelWell")
 except Exception as e :
     dataLog = pd.DataFrame([{
                 "Waktu Upload" : waktuUpload,
@@ -50,6 +38,22 @@ except Exception as e :
                 "Well" : "-",
                 "Status" : "⚠️ Gagal"
             }])
+    if os.path.exists(PATH_LOG) :
+        dataLog.to_csv("assets/log upload.csv", index=False, mode="a", header=False)
+    else :
+        dataLog.to_csv("assets/log upload.csv", index=False, mode="w")
+finally :
+    if button_kirim:
+        st.success("✅ Data berhasil diupload ke Google Sheets")
+    
+    dataLog = pd.DataFrame([{
+                "Waktu Upload" : waktuUpload,
+                "Production Oil & Gas" : "Sheet Oil Gas",
+                "WIP" : "Sheet WIP",
+                "Well" : "Sheet Well",
+                "Status" : "✅ Berhasil"
+            }])
+    
     if os.path.exists(PATH_LOG) :
         dataLog.to_csv("assets/log upload.csv", index=False, mode="a", header=False)
     else :
